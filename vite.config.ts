8202e45f453594406e181aed6349d9bf0c52a2f3
tkhs-0114 <tkhs.mail0114@gmail.com>
2025-09-28 21:48:37 +0900

    add: tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
   const base = command === "build" ? "/portfolio3/" : "/";
 
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     base,
   };
 });
